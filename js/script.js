9817345157ac9fc97658c9835488477b0a6c9027
thaianealm <cont.thaiane@gmail.com>
2022-12-22 13:03:07 -0300

    JavaScript update: new function - abstractions

--- js/script.js
+++ js/script.js
@@ -2,6 +2,16 @@ document.getElementById('checkAgeBtn').addEventListener('click', function(){
     checkAge();
 })
 
+function showMessage(){
+let divMessage = document.getElementById('message');
+
+        divMessage.style.display = 'block';
+
+        setTimeout(() => {
+            divMessage.style.display = 'none';
+         },6000);
+};
+
 function checkAge(){
     
     let aName = prompt('Digite seu nome: ');
@@ -13,17 +23,13 @@ function checkAge(){
     let age = currentYear - theYearOfBirth;
 
     if(aName == '' && theYearOfBirth == ''){
-        let divMessage = document.getElementById('message');
+        showMessage();
 
-        divMessage.style.display = 'block';
-
-        setTimeout(() => {
-            divMessage.style.display = 'none';
-         },6000);
-
-    }else if(age >= 18){
+    }else if(aName != null && theYearOfBirth != null && age >= 18){
         return alert(`${aName} tem ${age} anos de idade. ${aName} é maior de idade.`);
-    }else if(age < 18){
+    }else if(aName != null && theYearOfBirth != null && age < 18){
         return alert(`${aName} tem ${age} anos de idade. ${aName} é menor de idade`);
-    }
+    }else {
+        showMessage();
+    };
 };
